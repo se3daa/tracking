6bfdaa4cf9df52d89d9d7b0eb94a6a2f575c9301
se3daa <116446045+se3daa@users.noreply.github.com>
2023-05-06 02:08:43 +0300

    Add files via upload

---
+++ Tracking/data/issueDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tracking.models;
+
+namespace Tracking.data
+{
+    public class issueDBContext : DbContext
+    {
+        public issueDBContext(DbContextOptions<issueDBContext> options)
+               :base(options)
+        {
+        }
+      public DbSet<issue>issues { get; set; }
+    }
+}
---
+++ Tracking/models/issue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Tracking.models
+{
+    public class issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public priority priority { get; set; }
+        public issuetype issuetype { get; set; }
+        public DateTime created { get; set; }
+        public DateTime? completed { get; set; }
+    
+    }
+
+}
+ public enum priority
+{
+    Low, Medium, High
+}
+public enum issuetype
+{
+    Feature,Bug,Documentation
+}
